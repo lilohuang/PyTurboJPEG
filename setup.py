cd1579e42e3ebfbd7b7e8d309de49dd77241c1da
Lilo Huang <lilohuang@users.noreply.github.com>
2018-01-12 23:46:55 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup, find_packages
 setup(
     name='PyTurboJPEG',
+    author='Lilo Huang',
+    author_email='kuso.cc@gmail.com',
+    url='https://github.com/lilohuang/PyTurboJPEG',
     version='0.1',
     description='An experimental Python wrapper of TurboJPEG for decoding and encoding JPEG image.',
     license='MIT',
